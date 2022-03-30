c98f297a5312ede7468b3cd8fc01a15e08269ffd
Hiu Hoang <89955228+HiuHoang@users.noreply.github.com>
2022-03-30 12:21:34 +0700

    fix conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,28 +1,28 @@
 <?php
 
-Class Database{
- 
+class Database
+{
+
 	private $server = "54.165.107.150";
-	private $username = "root";
+	private $username = "ec2-user";
 	private $password = "123@123a";
-	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
 	protected $conn;
- 	
-	public function open(){
- 		try{
- 			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
- 			return $this->conn;
- 		}
- 		catch (PDOException $e){
- 			echo "There is some problem in connection: " . $e->getMessage();
- 		}
- 
-    }
- 
-	public function close(){
-   		$this->conn = null;
- 	}
- 
+
+	public function open()
+	{
+		try {
+			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
+			return $this->conn;
+		} catch (PDOException $e) {
+			echo "There is some problem in connection: " . $e->getMessage();
+		}
+	}
+
+	public function close()
+	{
+		$this->conn = null;
+	}
 }
 
 $pdo = new Database();
