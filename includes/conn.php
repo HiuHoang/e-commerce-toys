a79d35f9fe3cb2aad60c5aeb0154899dc44b52aa
Hiu Hoang <89955228+HiuHoang@users.noreply.github.com>
2022-03-30 13:30:37 +0700

    fix

--- includes/conn.php
+++ includes/conn.php
@@ -4,7 +4,7 @@ class Database
 {
 
 	private $server = "54.165.107.150";
-	private $username = "apache";
+	private $username = "root";
 	private $password = "123@123a";
 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
 	protected $conn;
